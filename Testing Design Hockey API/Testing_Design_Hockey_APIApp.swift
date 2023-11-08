a22774944926d998c0a77f70bbd8d92e4df5e791
macpadilla37 <89860555+macpadilla37@users.noreply.github.com>
2023-11-07 22:19:52 -0700

    Initial Commit

---
+++ Testing Design Hockey API/Testing_Design_Hockey_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_Design_Hockey_APIApp.swift
+//  Testing Design Hockey API
+//
+//  Created by Mac Padilla on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Testing_Design_Hockey_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Testing Design Hockey APIUITests/Testing_Design_Hockey_APIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Testing_Design_Hockey_APIUITestsLaunchTests.swift
+//  Testing Design Hockey APIUITests
+//
+//  Created by Mac Padilla on 11/7/23.
+//
+
+import XCTest
+
+final class Testing_Design_Hockey_APIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
